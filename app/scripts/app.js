8b2d0a5170bfa38e54c6337616d740c019cf7c51
branciard <francoisbranciard@gmail.com>
2016-07-09 22:42:33 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lepressoirdelacombeApp
+ * @description
+ * # lepressoirdelacombeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lepressoirdelacombeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lepressoirdelacombeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lepressoirdelacombeApp
+ */
+angular.module('lepressoirdelacombeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
